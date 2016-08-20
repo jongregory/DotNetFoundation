795e1720f8599ee97bd1c0d2b1552430be2f8fca
MMTDIGITAL\jon.g <jon.g@mmtdigital.co.uk>
2016-08-20 21:05:13 +0100

    Add the Convert class example

---
+++ NullHandlingExamples/ClassWithNulls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace NullHandlingExamples
+{
+    public class ClassWithNulls
+    {
+        public ClassWithNulls()
+        {
+            SampleDictionary = new Dictionary<string, string> { { Constants.NullValueKey, null } };
+        }
+        public object IamNull { get; set; }
+        public Dictionary<string, string> SampleDictionary { get; set; }
+    }
+}
--- NullHandlingExamples/Program.cs
+++ NullHandlingExamples/Program.cs
@@ -12,13 +12,13 @@ namespace NullHandlingExamples
         {
 
 
-            var sample = new Sample();
+            var classWithNulls = new ClassWithNulls();
 
             #region Example Property Access ToString Exception
 
             try
             {
-                var s = sample.IamNull.ToString();
+                var s = classWithNulls.IamNull.ToString();
             }
             catch (Exception ex)
             {
@@ -31,7 +31,7 @@ namespace NullHandlingExamples
 
             try
             {
-                var s = sample.SampleDictionary[Constants.NullValueKey].ToString();
+                var s = classWithNulls.SampleDictionary[Constants.NullValueKey].ToString();
             }
             catch (Exception ex)
             {
@@ -40,59 +40,52 @@ namespace NullHandlingExamples
 
             #endregion
 
-            #region null check samples
+            #region null check before ToString samples
 
             // 'AS' operator
             //https://msdn.microsoft.com/en-us/library/cscsdfbt.aspx
-            var asOperator = sample.IamNull as string;
+            var asOperator = classWithNulls.IamNull as string;
             Console.WriteLine("Null is returned by as with no exception : " + asOperator);
+
+            // Convert Class
+            // https://msdn.microsoft.com/en-us/library/system.convert(v=vs.110).aspx
+            var convertToString = Convert.ToString(classWithNulls.IamNull);
+            Console.WriteLine("Convert Class returns empty string if null no exception : " + convertToString);
+
 
             // '??' coalesce operator
             //https://msdn.microsoft.com/en-GB/library/ms173224.aspx
-            var coalesce  = sample.IamNull ?? "DefaultValueIfNull";
-            Console.WriteLine(coalesce);
+            var coalesce  = classWithNulls.IamNull ?? "DefaultValueIfNull";
+            Console.WriteLine("DefaultValue is returned if null : " + coalesce);
 
             // '?:' Conditional Operator 
             //https://msdn.microsoft.com/en-gb/library/ty67wk28.aspx
 
-            var conditional = sample.IamNull != null
-                ? sample.IamNull.ToString()
+            // ReSharper disable once MergeConditionalExpression
+            var conditional = classWithNulls.IamNull != null
+                ? classWithNulls.IamNull.ToString()
                 : "DefaultValueIfNull";
-            Console.WriteLine(conditional);
+            Console.WriteLine("DefaultValue is returned if null : " + conditional);
 
             // C# 6 and later null propagation operator
             //https://msdn.microsoft.com/en-GB/library/dn986595.aspx
 
             
-            var conditionalCSharp6 = sample.IamNull?.ToString();
+            var conditionalCSharp6 = classWithNulls.IamNull?.ToString();
             Console.WriteLine("Null is propagated and returned without an Exception" + conditionalCSharp6);
 
             // Can be combined with Coalesce
-            var conditionalCSharp6DefaultValue = sample.IamNull?.ToString() ?? "DefaultValueIfNull";
+            var conditionalCSharp6DefaultValue = classWithNulls.IamNull?.ToString() ?? "DefaultValueIfNull";
             Console.WriteLine("Default Value Returned without an Exception" + conditionalCSharp6DefaultValue);
 
             // Extension Method
-            var extensionMethod = sample.IamNull.ToStringOrEmpty();
+            // https://msdn.microsoft.com/en-gb/library/bb383977.aspx
+            var extensionMethod = classWithNulls.IamNull.ToStringOrEmpty();
             Console.WriteLine("String Empty is returned by as with no exception : " + extensionMethod);
 
             #endregion
 
             Console.ReadKey();
-        }
-
-        public class Sample
-        {
-            public Sample()
-            {
-                SampleDictionary = new Dictionary<string, string> {{Constants.NullValueKey, null}};
-            }
-            public object IamNull { get; set; }
-            public Dictionary<string, string> SampleDictionary { get; set; }
-        }
-
-        public static class Constants 
-        {
-            public static string NullValueKey = "NullValueKey";
         }
     }
 }
